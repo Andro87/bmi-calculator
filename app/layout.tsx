ab48853ee4928c9fafaba76187a2fe31a8b9585a
Andro87 <ggandolfo987@gmail.com>
2023-08-01 16:51:28 +0200

    [Accessibility] -Solve accessibility issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,7 @@ export const metadata: Metadata = {
     description: "Body Mass Index Calculator web app",
     viewport: {
         width: "device-width",
-        initialScale: 1,
-        maximumScale: 1
+        initialScale: 1
     }
 };
 
